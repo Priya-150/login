46c2fbc551008bee8c5c556f852e1f22945e798e
Priya-150 <159451684+Priya-150@users.noreply.github.com>
2025-03-25 23:41:45 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import SignupPage from "./SignupPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
